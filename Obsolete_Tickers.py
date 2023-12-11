5db8040d131b9efeb5ee5f2fc47d362782d9c21d
aaleen abbas syed <aaleensyed20@gmail.com>
2023-12-10 22:10:39 -0500

    50 day averaging

--- Obsolete_Tickers.py
+++ Obsolete_Tickers.py
@@ -9,7 +9,7 @@ from email.mime.text import MIMEText
 
 ##!<import pandas as pd
 ##!<import matplotlib.pyplot as plt
-##!<from datetime import datetime
+from datetime import datetime
 
 # Raw Package
 import numpy as np
@@ -33,83 +33,85 @@ class color:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 t = "    "
-ticker_list = ['NNDM' ,'OPTT' ,'CLSK' ,'LTRX' ,'MARA' ,
+ticker_list = ['NNDM' ,'OPTT' ,'RWLK','CLSK' ,'LTRX' ,'MARA' ,
                'PXLW' ,'VYNT' ,
-               'MOSYX' , 'TRT' ,'GMDA' ,'SCYX' ,
-               'EKSO','STXS','HUBC','KSCP','VSTM','INGN','AMSC','RMBL', 'ALIM','DTIL',]
-               #'RWLK', 'ITI','MRAM','ISDR','EYPT','CASS','BWAY','DAIO','CYBN',
-               #'DCTH','RELL','WATT','MCRB','SENS']
+               'MOSYX' , 'TRT' ,'GMDA' ,
+               #'SCYX' ,
+               'EKSO','STXS','HUBC','KSCP','VSTM','INGN','AMSC','RMBL', 'ALIM','DTIL',
+                'ITI','MRAM','ISDR','EYPT','CASS','BWAY','DAIO','CYBN',
+               'DCTH','RELL','WATT','MCRB','SENS','OTLY']
 
 def data_dl():
   data = [
           #yf.download(tickers = 'TTOO' ,period='1d', start='2020-05-05'), ## Reverse split on 10-13-22 sell on 8/30/23
-          yf.download(tickers = 'NNDM' ,period='1d', start='2020-06-02'),
+          yf.download(tickers = 'NNDM' ,period='1d', start=datetime(2020, 6, 2, 9, 30, 0)),
           #yf.download(tickers = 'RVP' ,period='1d', start='2020-06-02'),sell on 8/28/23
           #yf.download(tickers = 'IPWR' ,period='1d', start='2020-06-10'), sell on 8/29/23
-          yf.download(tickers = 'OPTT' ,period='1d', start='2020-06-16'),
+          yf.download(tickers = 'OPTT' ,period='1d', start=datetime(2020, 6, 16, 9, 30, 0)),
           #yf.download(tickers = 'LTBR' ,period='1d', start='2020-06-16'),sell on 8/29/23
           #yf.download(tickers = 'BIOC' ,period='1d', start='2020-06-16'), ## Reverse split 09-08-2020 sell on 8/24/23
           #yf.download(tickers = 'OCX' ,period='1d', start='2020-08-03'), ## reverse split 07/25-2023 sell on 8/28/23
           #yf.download(tickers = 'SMED' ,period='1d', start='2020-08-07'), ACQUISITION!!
          # yf.download(tickers = 'RIBT' ,period='1d', start='2020-08-25'), sell on 8/24/23
-          yf.download(tickers = 'CLSK' ,period='1d', start='2020-09-08'),
-          yf.download(tickers = 'LTRX' ,period='1d', start='2020-09-23'),
-          yf.download(tickers = 'MARA' ,period='1d', start='2020-09-25'),
+         yf.download(tickers = 'RWLK' ,period='1d',start=datetime(2020, 8, 25, 9, 30, 0)),
+          yf.download(tickers = 'CLSK' ,period='1d', start=datetime(2020, 9, 8, 9, 30, 0)),
+          yf.download(tickers = 'LTRX' ,period='1d', start=datetime(2020, 9, 23, 9, 30, 0)),
+          yf.download(tickers = 'MARA' ,period='1d', start=datetime(2020, 9, 25, 9, 30, 0)),
           #yf.download(tickers = 'SQNS' ,period='1d', start='2020-10-20'), sell on 8/07/23
           #yf.download(tickers = 'ONVO' ,period='1d', start='2020-10-21'), sell on 8/25/23
-          yf.download(tickers = 'PXLW' ,period='1d', start='2020-11-06'),
+          yf.download(tickers = 'PXLW' ,period='1d', start=datetime(2020, 11, 6, 9, 30, 0)),
           #yf.download(tickers = 'HYREQ' ,period='1d', start='2020-11-30'), sell on 3/9/23
           #yf.download(tickers = 'WWR' ,period='1d', start='2020-01-20'),  sell on 8/24/23
   
           #yf.download(tickers = 'SLGG' ,period='1d', start='2021-01-12'), sell on 9/08/23
           #yf.download(tickers = 'PRSO' ,period='1d', start='2021-01-12'), sell on 8/25/23
           #yf.download(tickers = 'ICCC' ,period='1d', start='2021-01-29'), sell on 8/28/23
-          yf.download(tickers = 'VYNT' ,period='1d', start='2021-02-25'),
+          yf.download(tickers = 'VYNT' ,period='1d', start=datetime(2021, 2, 25, 9, 30, 0)),
           #yf.download(tickers = 'URG' ,period='1d', start='2021-03-15'),  sell on 8/30/23
           #yf.download(tickers = 'IDXG' ,period='1d', start='2021-04-01'), sell on 8/24/23
           #yf.download(tickers = 'BOXL' ,period='1d', start='2021-04-26'), sell on 8/25/23
          # yf.download(tickers = 'WISA' ,period='1d', start='2021-05-12'), sell on 8/25/23
           #yf.download(tickers = 'PLUR' ,period='1d', start='2021-06-04'), sell on 8/28/23
           #yf.download(tickers = 'WATT' ,period='1d', start='2021-07-30'), sell on 9/06/23
-          yf.download(tickers = 'MOSYX' ,period='1d', start='2021-08-04'),
+          yf.download(tickers = 'MOSYX' ,period='1d', start=datetime(2021, 8, 4, 9, 30, 0)),
           #yf.download(tickers = 'APDN' ,period='1d', start='2021-10-12'), sell on 8/28/23
           #yf.download(tickers = 'CLXT' ,period='1d', start='2021-10-26'), Delisted
           #yf.download(tickers = 'AQMS' ,period='1d', start='2021-12-06'), sell on 8/30/23
           #yf.download(tickers = 'CBUS' ,period='1d', start='2021-12-22'), sell on 8/30/23
-          yf.download(tickers = 'TRT' ,period='1d', start='2021-12-22'),
-          yf.download(tickers = 'GMDA' ,period='1d', start='2022-03-01'),
-          yf.download(tickers = 'SCYX' ,period='1d', start='2022-03-16'),
-          yf.download(tickers = 'EKSO' ,period='1d', start='2023-01-10'),
-          yf.download(tickers = 'STXS' ,period='1d', start='2023-02-17'),
-          yf.download(tickers = 'HUBC' ,period='1d', start='2023-03-20'),
-          yf.download(tickers = 'KSCP' ,period='1d', start='2023-05-26'),
-          yf.download(tickers = 'VSTM' ,period='1d', start='2023-06-20'),
-          yf.download(tickers = 'INGN' ,period='1d', start='2023-06-27'),
-          yf.download(tickers = 'AMSC' ,period='1d', start='2023-07-27'),
-          yf.download(tickers = 'RMBL' ,period='1d', start='2023-08-01'),
+          yf.download(tickers = 'TRT' ,period='1d',interval='1h', start=datetime(2021, 12, 22, 9, 30, 0)),
+          yf.download(tickers = 'GMDA' ,period='1d', interval='1h',start=datetime(2023, 3, 1, 9, 30, 0)),
+          #yf.download(tickers = 'SCYX' ,period='1d', start='2022-03-16'), sell on 11/29/23
+          yf.download(tickers = 'EKSO' ,period='1d', interval='1h',start=datetime(2023, 1, 10, 9, 30, 0)),
+          yf.download(tickers = 'STXS' ,period='1d', interval='1h',start=datetime(2023, 2, 17, 9, 30, 0)),
+          yf.download(tickers = 'HUBC' ,period='1d', interval='1h',start=datetime(2023, 3, 20, 9, 30, 0)),
+          yf.download(tickers = 'KSCP' ,period='1d', interval='1h',start=datetime(2023, 5, 26, 9, 30, 0)),
+          yf.download(tickers = 'VSTM' ,period='1d', interval='1h',start=datetime(2023, 6, 20, 9, 30, 0)),
+          yf.download(tickers = 'INGN' ,period='1d', interval='1h',start=datetime(2023, 6, 27, 9, 30, 0)),
+          yf.download(tickers = 'AMSC' ,period='1d', interval='1h',start=datetime(2023, 7, 27, 9, 30, 0)),
+          yf.download(tickers = 'RMBL' ,period='1d', interval='1h',start=datetime(2023, 8, 1, 9, 30, 0)),
           #yf.download(tickers = 'WRAP' ,period='1d', start='2022-04-18'), sell on 8/29/23
           #yf.download(tickers = 'EOSE' ,period='1d', start='2022-03-17'), sell on 7/28/23
           #yf.download(tickers = 'VGFCQ' ,period='1d', start='2022-06-08'), sell on 3/09/23
-          yf.download(tickers = 'ALIM' ,period='1d', start='2023-08-15'),
-          yf.download(tickers = 'DTIL' ,period='1d', start='2023-08-18'),
+          yf.download(tickers = 'ALIM' ,period='1d', interval='1h',start=datetime(2023, 8, 15, 9, 30, 0)),
+          yf.download(tickers = 'DTIL' ,period='1d', interval='1h',start=datetime(2023, 8, 18, 9, 30, 0)),
           #yf.download(tickers = 'HGEN' ,period='1d', start='2023-07-10'), sell on 7/25/23
           #yf.download(tickers = 'APRN' ,period='1d', start='2023-02-03'), Sell APRN — 9/29/23
-          #yf.download(tickers = 'RWLK' ,period='1d', start='2020-08-25'),
-          #yf.download(tickers = 'ITI' ,period='1d', start='2023-08-24'),
-          #yf.download(tickers = 'MRAM' ,period='1d', start='2023-08-28'),
-          #yf.download(tickers = 'ISDR' ,period='1d', start='2023-08-29'),
-          #yf.download(tickers = 'EYPT' ,period='1d', start='2023-08-30'),
-          #yf.download(tickers = 'CASS' ,period='1d', start='2023-09-08'),
-          #yf.download(tickers = 'BWAY' ,period='1d', start='2023-09-15'),
-          #yf.download(tickers = 'DAIO' ,period='1d', start='2022-09-15'),
-          #yf.download(tickers = 'CYBN' ,period='1d', start='2023-11-01'),
-          #yf.download(tickers = 'DCTH' ,period='1d', start='2023-09-25'),
-          #yf.download(tickers = 'RELL' ,period='1d', start='2023-10-04'),
+          yf.download(tickers = 'ITI' ,period='1d', interval='1h',start=datetime(2023, 9, 24, 9, 30, 0)),
+          yf.download(tickers = 'MRAM' ,period='1d', interval='1h',start=datetime(2023, 8, 28, 9, 30, 0)),
+          yf.download(tickers = 'ISDR' ,period='1d', interval='1h',start=datetime(2023, 8, 29, 9, 30, 0)),
+          yf.download(tickers = 'EYPT' ,period='1d', interval='1h',start=datetime(2023, 8, 30, 9, 30, 0)),
+          yf.download(tickers = 'CASS' ,period='1d', interval='1h',start=datetime(2023, 9, 8, 9, 30, 0)),
+          yf.download(tickers = 'BWAY' ,period='1d', interval='1h',start=datetime(2023, 9, 15, 9, 30, 0)),
+          yf.download(tickers = 'DAIO' ,period='1d', interval='1h',start=datetime(2023, 9, 15, 9, 30, 0)),
+          yf.download(tickers = 'DCTH' ,period='1d', interval='1h',start=datetime(2023, 9, 25, 9, 30, 0)),
+          yf.download(tickers = 'RELL' ,period='1d', interval='1h',start=datetime(2023, 10, 4, 9, 30, 0)),
+          yf.download(tickers = 'CYBN' ,period='1d', interval='1h',start=datetime(2023, 11, 1, 9, 30, 0)),
           # yf.download(tickers = 'AFIB' ,period='1d', start='2022-03-09'), sell on 11/09/2023
           # yf.download(tickers = 'CDLX' ,period='1d', start='2023-09-01'), sell on 11/09/2023
-          #yf.download(tickers = 'WATT' ,period='1d', start='2023-11-02'),
-          #yf.download(tickers = 'MCRB' ,period='1d', start='2023-11-03'),
-          #yf.download(tickers = 'SENS' ,period='1d', start='2023-11-14'),
+          yf.download(tickers = 'WATT' ,period='1d', interval='1h',start=datetime(2023, 11, 3, 9, 30, 0)),
+          yf.download(tickers = 'MCRB' ,period='1d', interval='1h',start=datetime(2023, 11, 3, 9, 30, 0)),
+          yf.download(tickers = 'SENS' ,period='1d', interval='1h',start=datetime(2023, 11, 14, 9, 30, 0)),
+          yf.download(tickers = 'OTLY' ,period='1d', interval='1h',start=datetime(2023, 12, 1, 11, 30, 0))
           ]
   return data
 def plot_graph():
@@ -193,7 +195,63 @@ def plot_graph_hike():
   
   fig.show()
   
+def Avg():
+   content = ""
+   for ticker, value in zip(ticker_list, data_dl()):
+      sum1=0
+      sum2=0
+      indexReached=0
+      tickerError=""
+      #print("DBGMIR", ticker, value.index[0].hour)
+      if(value.index[0].hour==0):
+        for i in range(0,49):
+            try:
+                sum1 += value['Open'][i]
+                sum2 +=value['Open'][-i-1]
+            except IndexError:
+                tickerError = ticker
+                indexReached = i
+                continue
+        Start_Average = round(sum1/50,2)
+        End_Average = round(sum2/50,2)
+      else:
+         print("DBGMIR", ticker, len(value['Open']))
+         #for i in range(0,len(value["Open"]),24):
+         for i in range(0,49*24,24):
+            try:
+                sum1 += value['Open'][i]
+                sum2 +=value['Open'][-i-1]
+            except IndexError:
+                tickerError = ticker
+                indexReached = i
+                break
+         Start_Average = round(sum1/50,2)
+         End_Average = round(sum2/50,2)
+      
+      
+      if tickerError == ticker:
+         content += ( "<p style = 'font-size: 25px;'>[Obsolete( <b>" + tickerError + "</b>)] ticker_Error index[ "+str(indexReached)+"] Averages: " +
+                  "   <br>"+t+t+"   Starting Average is " + str(Start_Average) +
+                  "   <br>"+t+t+"   End_Average is " + str(End_Average) +
+                  "<br>"
+                  )
+      else:
+         content += ( "<p style = 'font-size: 25px;'>[Obsolete( <b>" + ticker + "</b>)] Averages: " +
+                  "   <br>"+t+t+"   Starting Average is " + str(Start_Average) +
+                  "   <br>"+t+t+"   End_Average is " + str(End_Average) +
+                  "<br>"
+                  )
+         
+      
+   return content
+      
+    
+      
+    
 
+         
+      
+      
 
 def Drop():
     content = ""
@@ -207,10 +265,11 @@ def Drop():
         #(New Price - Old Price)/Old Price and then multiply that number by 100
         roc = round(((latest_market_price - first_opening_price) / first_opening_price) * 100,2);    
 
+        print (ticker, value, first_opening_price)
         #if latest_market_price < first_opening_price * 0.8:
         if roc < 0:
             price_drop = round(first_opening_price - latest_market_price,2)
-            print ( "[Obsolete( "+ color.BOLD + ticker + color.END + ")] price drop: " + str(roc) + "%")
+            #print ( "[Obsolete( "+ color.BOLD + ticker + color.END + ")] price drop: " + str(roc) + "%")
             content += ( "<p style = 'font-size: 25px;'>[Obsolete( <b>" + ticker + "</b>)] price drop: " + str(roc) + "%" +
                   "   <br>"+t+t+"   Your first opening price was " + str(first_opening_price) +
                   "   <br>"+t+t+"   Your latest market price is " + str(latest_market_price) +
@@ -226,8 +285,8 @@ def Hike():
     for ticker, value in zip(ticker_list, data_dl()):
         first_opening_price = round(value['Open'][0],2)
         latest_market_price = round(value['Close'][-1],2)
-
-        
+        print(str(ticker) + " " + str(value.index))
+        #print (ticker, value)
         limit_price = round(first_opening_price * 1.6, 2)
 
         #(New Price - Old Price)/Old Price and then multiply that number by 100
@@ -235,13 +294,14 @@ def Hike():
 
         #if latest_market_price < first_opening_price * 0.8:
         if roc > 0:
-            print ( "[Obsolete( "+ color.BOLD + ticker + color.END + ")] price hike: " + str(roc) + "%")
+            #print ( "[Obsolete( "+ color.BOLD + ticker + color.END + ")] price hike: " + str(roc) + "%")
             price_hike = round(latest_market_price - first_opening_price,2)
             content += ("<p style = 'font-size: 25px;'>[Obsolete( <b>" + ticker + "</b>)] price hike: " + str(roc) + "%" + 
-                  "   <br>"+t+t+"  Your first opening price was " + str(first_opening_price) +
+                  "   <br>"+t+t+"  Your first opening price was " + str(first_opening_price) +" time: " +str(value.index[0])+
                   "   <br>"+t+t+"  Your latest market price is " + str(latest_market_price) +
                   "   <br>"+t+t+"  Your price hike is " + str(price_hike) +
                   "   <br>"+t+t+"  Your limit price is " + str(limit_price) +
+                  
                   "<br>")
     return content   
     
@@ -338,9 +398,10 @@ def send_email(email):
   msg['To'] = receiver_email
 
   content = ""
-  content += Hike() + "<br>"
+  content = Avg()
+  #content += Hike() + "<br>"
   
-  content += Drop()
+  #content += Drop()
   
 
    
